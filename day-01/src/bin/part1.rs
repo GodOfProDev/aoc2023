a58e7b060cdbd875e849e91bf25d99ae5f1614b7
GodOfPro <1387ilia@gmail.com>
2023-12-03 22:31:05 +0330

    Finish Part 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,3 +1,52 @@
+use std::fs;
+
 fn main() {
-    println!("part1")
+    println!("part1");
+    let file_path = "E:\\Programming\\rustProjects\\aoc2023\\day-01\\src\\bin\\input1.txt";
+
+    let mut input = fs::read_to_string(file_path).unwrap();
+    input.push('\r');
+
+    let mut vec: Vec<Vec<char>> = Vec::new();
+    let mut tec: Vec<char> = Vec::new();
+
+    for x in input.chars() {
+        if char::is_numeric(x) {
+            tec.push(x);
+        }
+
+        if x == '\r' {
+            vec.push(tec.clone());
+            tec.clear();
+        }
+    }
+
+    for y in vec.iter_mut() {
+        if y.len() == 1 {
+            y.push(*y.get(0).unwrap());
+        }
+    }
+
+    for y in vec.iter_mut() {
+        if y.len() > 2 {
+            let k = y.clone();
+            let x = k.get(0).unwrap();
+            let z = k.get(y.len() - 1).unwrap();
+
+            y.clear();
+
+            y.push(*x);
+            y.push(*z);
+        }
+    }
+
+    let mut sum: u32 = 0;
+
+    for j in vec.iter() {
+        let str: String = j.into_iter().collect();
+        let strint = str.parse::<u32>().unwrap();
+        sum += strint;
+    }
+
+    dbg!(sum);
 }
